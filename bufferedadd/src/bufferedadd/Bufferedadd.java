fcc2a590607d58958214ff64fe2c7280189ce559
hassan abbas <hassan abbas@DESKTOP-RIL9KUS>
2022-02-14 12:04:09 +0530

    add

---
+++ bufferedadd/src/bufferedadd/Bufferedadd.java
@@ -0,0 +1,24 @@
+package bufferedadd;
+
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+public class Bufferedadd {
+
+
+
+	public static void main(String[] args) throws Exception {
+			// TODO Auto-generated method stub
+			InputStreamReader hell = new InputStreamReader(System.in);
+			BufferedReader hello = new BufferedReader(hell);
+			System.out.println("Put ur first no. to add");
+			int c = Integer.parseInt(hello.readLine());
+			System.out.println("Second no. plz");
+			int b = Integer.parseInt(hello.readLine());
+			int A = b+c;
+			System.out.println("Your sum for the no.s you provided is:\n"+A);
+		}
+
+	}
+
+}
