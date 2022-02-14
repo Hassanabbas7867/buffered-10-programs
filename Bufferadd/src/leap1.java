31f4f6f274c89a31b83989fcba8aa032424c8679
Hassanabbas7867 <98871037+Hassanabbas7867@users.noreply.github.com>
2022-02-14 12:50:58 +0530

    day5

---
+++ Bufferadd/src/leap1.java
@@ -0,0 +1,21 @@
+
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+public class leap1 {
+
+	public static void main(String[] args) throws IOException
+	{
+		int year;
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println("Enter a year");
+		year = Integer.parseInt(in.readLine());
+	      if (((year % 4 == 0) && (year % 100!= 0)) || (year%400 == 0))
+	         System.out.println("leap year");
+	      else
+	         System.out.println("not a leap year");
+
+	}
+
+}
