34f5df0d5583f7e64b9cafcd9962318a1ac332be
hassan abbas <hassan abbas@DESKTOP-RIL9KUS>
2022-02-14 12:02:45 +0530

    day5

---
+++ Bufferadd/src/Bufferadd.java
@@ -0,0 +1,25 @@
+
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class Bufferadd {
+    public static void main(String args[]) throws IOException
+    {
+
+        int num1,num2, sum;
+
+        InputStreamReader is = new InputStreamReader(System.in);
+        BufferedReader br = new BufferedReader(is);
+
+        System.out.print("Enter two numbers:");
+        num1 = Integer.parseInt(br.readLine());
+        num2 = Integer.parseInt(br.readLine());
+
+        sum = num1 + num2;
+
+        System.out.println("Sum is:" + sum);
+
+    }
+}
